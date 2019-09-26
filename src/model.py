8e0733d93a9f795b249b81d2078a063a07b4ab53
Neev Parikh <neev_parikh@brown.edu>
2019-09-26 13:20:17 -0400

    Formatted and added all eps

--- src/model.py
+++ src/model.py
@@ -1,11 +1,12 @@
 from torch import nn
+import numpy as np
 from torch.nn import functional as F
 import torch.optim as optim
 from tqdm import tqdm
-import glob 
+import glob
+
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(5, 5)
@@ -16,20 +17,29 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
+
 net = Net()
 
 optimizer = optim.Adam(net.parameters(), lr=1e-3)
 
 criterion = nn.MSELoss()
 
+
+states = np.array([])
+actions = np.array([])
+rewards = np.array([])
+
 for ep_filepath in tqdm(glob.glob("./data/episode*.npz")):
     data = np.load(ep_filepath)
+    np.append(states, data['states'])
+    np.append(actions, data['actions'])
+    np.append(rewards, data['rewards'])
 
-for i in len(states)-1:
+for i in len(states) - 1:
     optimizer.zero_grad()
     output = net(np.append(states[i], actions[i]))
 
-    loss = criterion(output, np.append(states[i+1], rewards[i]))
+    loss = criterion(output, np.append(states[i + 1], rewards[i]))
     print("loss", loss)
     loss.backward()
     optimizer.step()
