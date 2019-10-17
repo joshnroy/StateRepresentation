4721e151a42f2261126910cd05f87008e3e01c53
Neev Parikh <neev_parikh@brown.edu>
2019-10-17 13:50:25 -0400

    more tries with terminal

--- src/model.py
+++ src/model.py
@@ -6,59 +6,75 @@ import numpy as np
 import glob
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+EPOCHS = 200
+train = True
+BATCH_SIZE = 10
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(5, 5)
-        self.fc2 = nn.Linear(5, 6)
+        self.fc2 = nn.Linear(5, 48)
+        self.fc3 = nn.Linear(48, 48)
+        self.fc4 = nn.Linear(48, 48)
+        self.fc5 = nn.Linear(48, 6)
 
     def forward(self, x):  # pylint: disable=arguments-differ
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
         return x
 
+
+
+# def weightedL1Loss(output, label, weights):
+#     output - label
 
 def main():
     net = Net()
     net.to(device)
 
     optimizer = optim.Adam(net.parameters(), lr=1e-3)
-
     criterion = nn.L1Loss()
-    train = True
+
     if train:
-        epochs = 300
-        for epoch in range(epochs):
-            for ep_filepath in glob.glob("./data/**/episode*.npz"):
-                data = np.load(ep_filepath)
+        # Batch up the data
+        dataset = []
+        for ep_filepath in glob.glob("./data/**/episode*.npz"):
+            data = np.load(ep_filepath)
+            states = data['states']
+            actions = data['actions']
+            rewards = data['rewards']
+            terminal = data['terminal'] * 100000
+            episode_batch = []
+            for i in range(len(actions)):
+                transition = np.append(states[i], actions[i])
+                transition = np.append(transition, states[i + 1])
+                transition = np.append(transition, rewards[i])
+                transition = np.append(transition, terminal[i])
+                episode_batch.append(transition.transpose())
+            dataset.append(np.array(episode_batch))
+        dataset = np.vstack(dataset)
 
-                states = data['states']
-                actions = data['actions']
-                rewards = data['rewards']
-                terminal = data['terminal']
-
+        for epoch in range(EPOCHS):
+            np.random.shuffle(dataset)
+            for i in range(0, dataset.shape[0] - BATCH_SIZE, BATCH_SIZE):
+                batch = np.take(dataset, np.arange(i, i + BATCH_SIZE), axis=0)
                 optimizer.zero_grad()
-                input_tensor = torch.from_numpy(
-                    np.append(states[:-1, :],
-                              np.expand_dims(actions, axis=1),
-                              axis=1).astype(np.float32))
-                input_tensor = input_tensor.to(device)
+                input_tensor = torch.from_numpy(batch[:, 0:5].astype(
+                    'float32'))
                 output = net(input_tensor)
-
-                label = torch.from_numpy(
-                    np.append(np.append(states[1:, :],
-                                        np.expand_dims(rewards, axis=1),
-                                        axis=1).astype(np.float32),
-                              np.expand_dims(terminal, axis=1),
-                              axis=1).astype(np.float32))
+                label = torch.from_numpy(batch[:, 5:].astype('float32'))
                 label = label.to(device)
                 loss = criterion(output, label)
                 loss.backward()
                 optimizer.step()
-            print("loss", loss)
-            if loss < 0.0005:
+
+            print("epoch: {:04d}, loss: {:02.6f}".format(epoch, loss.item()))
+            if loss < 1e-6:
                 break
 
         torch.save(net, "net.pth")
@@ -69,11 +85,12 @@ def main():
     states = data['states']
     actions = data['actions']
     rewards = data['rewards']
-    terminal = data['terminal']
+    terminal = data['terminal'] * 100000
     input_tensor = torch.from_numpy(
         np.append(states[-2, :], actions[-1]).astype(np.float32)).to(device)
     print(net(input_tensor))
     print(states[-1, :], rewards[-1], terminal[-1])
+
 
 if __name__ == '__main__':
     main()
